919401d0f9a452bae0477febf34f32452864acb9
Nikolay Masson <nick.masson@yandex.ru>
2024-10-03 23:21:54 +0200

    feat: add regs and alpha notice links to footer

--- src/App.vue
+++ src/App.vue
@@ -39,11 +39,20 @@ const onReturnToMain = () => {
     </main>
     <footer class="footer bg-base-300 p-4">
       <p class="inline">Made with ❤️ by <b>Nikolai Masson</b></p>
-      <span class="text-center place-self-end">
+      <div class="text-center flex justify-between place-self-end">
         <a class="text-center" href="https://github.com/Niksson/333centurion">
           <FontAwesomeIcon icon="fa-brands fa-github" /> Github
         </a>
-      </span>
+        <a
+          class="text-center"
+          href="https://docs.google.com/document/d/1E0HR36tRbCK6hXiwavSnsNiJxXqOYpcEqVpdWdf8cXU/edit?usp=sharing"
+        >
+          Regulations
+        </a>
+        <a class="text-center" href="https://github.com/Niksson/333centurion#alpha-status">
+          Alpha status
+        </a>
+      </div>
     </footer>
   </div>
 </template>
